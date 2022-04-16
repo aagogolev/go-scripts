a7e021823169e8a8fbcbe08de280e085106951fa
aleksandr.gogolev <aleksandr.gogolev@simbirsoft.com>
2022-04-16 20:20:01 +0300

    add script to get files and folders recursively

---
+++ file-system/get-files/get-files.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func reportPanic() {
+	p := recover()
+	if p == nil {
+		return
+	}
+	err, ok := p.(error)
+	if ok {
+		fmt.Println(err)
+	} else {
+		panic(p)
+	}
+}
+
+func scanDirectory(path string) {
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
+
+		if file.IsDir() {
+			scanDirectory(filePath)
+		} else {
+			fmt.Println(filePath)
+		}
+	}
+}
+
+func main() {
+
+	if len(os.Args) != 2 {
+		println("Invalid arguments")
+		os.Exit(1)
+	}
+	defer reportPanic()
+	scanDirectory(os.Args[1])
+}
